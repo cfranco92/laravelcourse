b687d5273b2b17ef9656964fa57375a186af19ea
Cristian Franco <cfrancobedoya@gmail.com>
2020-08-21 08:26:13 -0500

    Adding activity 2

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,7 +9,8 @@
     <title>@yield('title','Home Page')</title> 
 
     <!-- Styles -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous"> -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/customStyle.css') }}">
 
 </head> 
