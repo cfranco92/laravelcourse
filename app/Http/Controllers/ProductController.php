0792d26d30868a1f50ca4af0377aa6be92aeb06b
Cristian Franco <cfrancobedoya@gmail.com>
2020-08-21 17:17:15 -0500

    Adding activity 2.b

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function show($id) {
+        $data = []; //to be sent to the view
+
+        $listProducts = array();
+        $listProducts[121] = array("name"=>"Tv samsung", "price"=>"1000");
+        $listOfSizes = array("XS", "S", "M", "L", "XL");
+
+        $data["title"] = $listProducts[$id]["name"];
+        $data["product"] = $listProducts[$id];
+        $data["sizes"] = $listOfSizes;
+        return view('product.show')->with("data", $data);
+    }
+}
