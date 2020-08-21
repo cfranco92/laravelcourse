f5a001fe697b8dc9c7f2365c47279a9b7ebfd98f
Cristian Franco <cfrancobedoya@gmail.com>
2020-08-21 08:11:31 -0500

    Adding activity 1

---
+++ resources/views/home/contact.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class=card-header>Contact</div>
+
+                <div class="card-body">
+                    Name: Cristian Franco Bedoya <br>
+                    Address: Sabaneta, Antioquia <br>
+                    Phone: 3052407519 <br>
+                    Email: cfranc11@eafit.edu.co <br>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
