1b15bccbf62622b576f33364fc6fafa623047cad
Cristian Franco <cfrancobedoya@gmail.com>
2020-10-10 00:36:25 -0500

    Adding new workshop, A. A "basic" API system.

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/products', 'Api\ProductApi@index')->name("api.product.index");
+Route::get('/products/{id}', 'Api\ProductApi@show')->name("api.product.show");
